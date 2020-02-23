935d3845364bf2fff79946b55022583d32567827
= <edsondiasalves@hotmail.com>
2020-02-23 00:22:06 +0000

    Implementing widget test structure

--- test/widget/dictionary_search_widget_test.dart
+++ test/widget/dictionary_search_widget_test.dart
@@ -1,30 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:portuguese_dictionary/main.dart';
+import 'package:portuguese_dictionary/service/definition_service.dart';
+import 'package:portuguese_dictionary/widget/bloc/bloc.dart';
+import 'package:portuguese_dictionary/widget/dictionary_search_widget.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  group('Dictionary Search Widget', () {
+    SearchBloc bloc;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    setUp(() {
+      bloc = SearchBloc(definitionService: DefinitionService());
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    testWidgets('Shows a loading', (WidgetTester tester) async {
+      await tester.pumpWidget(BlocProvider.value(
+        value: bloc,
+        child: DictionarySearchWidget(),
+      ));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      final scaffold = find.byType(Scaffold);
+
+      expect(scaffold, findsOneWidget);
+    });
   });
 }
