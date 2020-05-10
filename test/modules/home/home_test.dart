79062a465794427e7ec843eef750341847324a97
= <edsondiasalves@hotmail.com>
2020-05-10 21:20:27 +0100

    Remove unused import

--- test/modules/home/home_test.dart
+++ test/modules/home/home_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:portuguese_dictionary/model/model.dart';
 import 'package:portuguese_dictionary/modules/home/bloc/bloc.dart';
 import 'package:portuguese_dictionary/modules/home/home.dart';
 import 'package:portuguese_dictionary/modules/search/bloc/bloc.dart';
