160237dc135ba132bc15b07c45f7256ac42921e8
= <edsondiasalves@hotmail.com>
2020-07-12 16:05:01 +0100

    Removing unnecessary code

--- lib/bottom_bar.dart
+++ lib/bottom_bar.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/material.dart';
 
-enum TabItem {
-  red,
-  green,
-  blue,
-}
-
-Map<TabItem, String> tabName = {
-  TabItem.red: 'red',
-  TabItem.green: 'green',
-  TabItem.blue: 'blue',
-};
-
-Map<TabItem, MaterialColor> tabColors = {
-  TabItem.red: Colors.red,
-  TabItem.green: Colors.green,
-  TabItem.blue: Colors.blue,
-};
-
 class BottomBar extends StatelessWidget {
   final int currentTab;
   final ValueChanged<int> onTabChange;
