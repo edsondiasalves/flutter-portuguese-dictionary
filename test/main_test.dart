fe3afc51c8ff7a0ac95f9e03acc296edf31e9a36
Edson Moisinho <edson.moisinhos@ctw.bmwgroup.com>
2021-03-28 20:49:46 +0100

    Mocking firebase

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,22 +1,20 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:portuguese_dictionary/main.dart' as dictionary_main;
 import 'package:portuguese_dictionary/modules/intro/intro.dart';
 import 'package:portuguese_dictionary/routes.dart';
 
-class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
-
-class MockFirebaseApp extends Mock implements FirebaseApp {}
+import '../lib/multitab.dart';
+import 'firebase_mock.dart';
 
 void main() {
+  setupFirebaseAuthMocks();
   FirebaseApp firebaseApp;
 
-  setUp(() async {
+  setUpAll(() async {
     TestWidgetsFlutterBinding.ensureInitialized();
-    firebaseApp = MockFirebaseApp();
+    firebaseApp = await Firebase.initializeApp();
   });
 
   group('Main', () {
@@ -28,7 +26,7 @@ void main() {
       }
     });
 
-    testWidgets('Shows the intro main page', (WidgetTester tester) async {
+    testWidgets('Calls the intro main page', (WidgetTester tester) async {
       //Arrange
       await tester.runAsync(() async {
         await tester.pumpWidget(
@@ -45,8 +43,28 @@ void main() {
 
         //Assert
         expect(find.byType(Intro), findsOneWidget);
+        expect(find.byType(Multitab), findsNothing);
+        expect(find.byType(CircularProgressIndicator), findsNothing);
+      });
+    });
+
+    testWidgets('Calls the Multitab page', (WidgetTester tester) async {
+      //Arrange
+      await tester.runAsync(() async {
+        await tester.pumpWidget(
+          dictionary_main.Main(
+            initialRoute: Routes.multitab,
+            firebaseApp: Future.value(firebaseApp),
+          ),
+        );
+
+        //Assert
+        expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+        await tester.pump(Duration(seconds: 1));
+        expect(find.byType(Intro), findsNothing);
+        expect(find.byType(CircularProgressIndicator), findsNothing);
       });
     });
   });
 }
-
